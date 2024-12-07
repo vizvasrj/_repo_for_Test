878309583be69c541c930bffdbd8d8843e31e5ba
vizvasrj <vizvasrj@gmail.com>
2024-12-07 22:29:47 +0530

    ne db

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"log"
+	"net/http"
 	"os"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -21,10 +23,18 @@ func main() {
 	g := cli.Database("test").Collection("insidetest")
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
-	_, err = g.InsertOne(ctx, bson.D{{"time", time.Now()}})
+	_, err = g.InsertOne(ctx, bson.D{{"time", time.Now().In(time.FixedZone("UTC+5:30", 19800))}})
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+	router.Run(":3000")
 
 }
 
